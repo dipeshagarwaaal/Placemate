c99117cb6a10e3279613ce8267ca0fbb10f81a3b
MoinMN <moinnaik45@eng.rizvi.edu.in>
2024-08-31 23:13:17 +0530

    magement can create tpo admin

--- frontend/src/components/students/Account.jsx
+++ frontend/src/components/students/Account.jsx
@@ -1,176 +1,249 @@
-import React, { useState, useEffect } from 'react';
-import Table from 'react-bootstrap/Table';
-import Button from 'react-bootstrap/Button';
+import React from 'react';
 import axios from 'axios';
+import { useState, useEffect } from 'react';
+import { FaRegSave } from 'react-icons/fa';
+import { RiKeyFill } from "react-icons/ri";
 import Form from 'react-bootstrap/Form';
 import FloatingLabel from 'react-bootstrap/FloatingLabel';
-import { GrFormAdd } from "react-icons/gr";
 import Toast from '../Toast';
 
-function AddTPO() {
-  // tpo users store here
-  const [users, setUsers] = useState([]);
+function Account() {
+  document.title = 'CPMS | Account';
+  const BASE_URL = "http://localhost:4518";
+
+  const [data, setData] = useState({});
   const [loading, setLoading] = useState(true);
+  // console.log(data)
+  useEffect(() => {
+    const fetchUserData = async () => {
+      try {
+        const token = localStorage.getItem('token');
+        const response = await axios.get('http://localhost:4518/student/detail', {
+          headers: {
+            'Authorization': `Bearer ${token}`,
+          }
+        });
+        setData(response.data);
+        setLoading(false);
+      } catch (error) {
+        console.log("Account.jsx => ", error);
+        setLoading(false);
+      }
+    }
+    fetchUserData();
+  }, []);
+
+  const handleBasicDetailChange = (e) => setData({ ...data, [e.target.name]: e.target.value });
+
+  const handleBasicDetailSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const token = localStorage.getItem('token');
+      const response = await axios.post("http://localhost:4518/student/update-basic-detail",
+        data,
+        {
+          headers: {
+            'Authorization': `Bearer ${token}`,
+          }
+        }
+      );
+      setToastMessage(response.data.msg);
+      setShowToast(true);
+    } catch (error) {
+      setToastMessage(error.message);
+      setShowToast(true);
+      console.log("handleBasicDetailSubmit => ", error);
+    }
+  }
+
+  const handlePhotoChange = async (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      const formData = new FormData();
+      formData.append('profileImgs', file);
+      formData.append('userId', data.id);
+
+      try {
+        // const token = localStorage.getItem('token');
+        const response = await axios.post("http://localhost:4518/student/upload-photo", formData
+          // , {
+          // headers: {
+          //   'Authorization': `Bearer ${token}`,
+          //   'Content-Type': 'multipart/form-data'
+          // }}
+        );
+        setToastMessage(response.data.msg);
+        setShowToast(true);
+        // updating data so dont require refresh
+        setData({ ...data, profile: response.data.file });
+      } catch (error) {
+        setToastMessage(error.message);
+        setShowToast(true);
+        console.error('Error uploading photo:', error);
+      }
+    }
+  }
 
   // useState for toast display
   const [showToast, setShowToast] = useState(false);
   const [toastMessage, setToastMessage] = useState('');
 
-  const fetchUserDetails = async () => {
-    try {
-      const response = await axios.get("http://localhost:4518/management/tpo-users", {
-        headers: {
-          Authorization: `Bearer ${localStorage.getItem("token")}`, // Assuming token is stored in localStorage
-        }
-      });
-
-      if (response.data) {
-        setUsers(response.data.tpoUsers);
-      } else {
-        console.warn('Response does not contain tpoUsers:', response.data);
-      }
-    } catch (error) {
-      console.error("Error fetching user details", error);
-    } finally {
-      setLoading(false);
-    }
-  };
-  fetchUserDetails();
-
-  const [formOpen, setFormOpen] = useState(false);
-  const [data, setData] = useState({
-    name: "",
-    email: "",
-    number: "",
-    password: ""
+  // useState for passwords
+  const [passData, setPassData] = useState({
+    oldpass: "",
+    newpass: "",
+    newcfmpass: "",
+    error: "",
   });
 
-  const handleDataChange = (e) => setData({ ...data, [e.target.name]: e.target.value });
+  const handlePassChange = (e) => setPassData({ ...passData, [e.target.name]: e.target.value })
 
-  const handleDeleteUser = async (email) => {
-    const response = await axios.post("http://localhost:4518/management/deletetpo",
-      { email },
-      {
-        headers: {
-          Authorization: `Bearer ${localStorage.getItem("token")}`,
-        }
-      }
-    );
-    if (response.data) {
-      setToastMessage(response.data.msg);
-      setShowToast(true);
-      fetchUserDetails();
-    }
-  }
-
-  const handleSubmit = async (e) => {
+  // password update
+  const handlePassUpdate = (e) => {
     e.preventDefault();
+
+    if (!passData.oldpass || !passData.newpass || !passData.newcfmpass) return setPassData({ ...passData, error: "All Field Requied!" });
+
+    // if newpass and newcfmpass is matching or not
+    if (passData.newpass != passData.newcfmpass) return setPassData({ ...passData, error: "new password and confirm new password didn't matched" });
+
     try {
-      const response = await axios.post("http://localhost:4518/management/addtpo",
-        data,
+      const token = localStorage.getItem('token');
+      const response = axios.post("http://localhost:4518/student/changepass",
+        passData,
         {
           headers: {
-            Authorization: `Bearer ${localStorage.getItem("token")}`,
+            'Authorization': `Bearer ${token}`,
           }
         }
       );
-      if (response.data) {
-        setToastMessage(response.data.msg);
-        setShowToast(true);
-        fetchUserDetails();
-      }
+      response.then((res) => setPassData({ ...passData, error: res.data }));
     } catch (error) {
-      console.log("handleSubmit => AddTPO.jsx ==> ", error);
+      console.log("Account.jsx updatepass =>", error);
+      setPassData({ ...passData, error: error.message });
     }
   }
+  // for password update toasts
+  useEffect(() => {
+    if (passData.error) {
+      setToastMessage(passData.error);
+      setShowToast(true);
+    }
+  }, [passData.error]);
+
+  const skeletonStyle = {
+    backgroundColor: '#e0e0e0',
+    borderRadius: '4px',
+    marginBottom: '10px',
+    height: '20px',
+  };
+  
+  const skeletonRowStyle = {
+    display: 'flex',
+    alignItems: 'center',
+  };
+  
+  const skeletonCellStyle = {
+    flex: 1,
+    height: '20px',
+    margin: '5px',
+    backgroundColor: '#ccc',
+    borderRadius: '4px',
+  };
+  
 
 
   return (
     <>
-      {/*  any message here  */}
-      < Toast
-        show={showToast}
-        onClose={() => setShowToast(false)}
-        message={toastMessage}
-        delay={3000}
-        position="top-center"
-      />
-    
-      <div className='ml-60'>
-        {
-          loading ? (
-            <div className="flex justify-center h-72 items-center">
-              <i className="fa-solid fa-spinner fa-spin text-3xl" />
-            </div>
-          ) : (
-            <Table striped bordered hover className='bg-white p-6 rounded-lg shadow-md w-full'>
-              <thead>
-                <tr>
-                  <th>Sr. No.</th>
-                  <th>Name</th>
-                  <th>Email</th>
-                  <th>Phone Number</th>
-                  <th>Date of Joining</th>
-                  <th>Delete User</th>
-                </tr>
-              </thead>
-              <tbody>
-                {users.length > 0 ? (
-                  users.map((user, index) => (
-                    <tr key={user.email}>
-                      <td>{index + 1}</td>
-                      <td>{user.name}</td>
-                      <td>{user.email}</td>
-                      <td>{user.number}</td>
-                      <td>{new Date(user.createdAt).toLocaleDateString()}</td>
-                      <td>
-                        <i className="fa-regular fa-trash-can text-2xl cursor-pointer hover:text-red-500" onClick={() => handleDeleteUser(user.email)} />
-                      </td>
-                    </tr>
-                  ))
-                ) : (
-                  <tr>
-                    <td colSpan="5">No users found</td>
-                  </tr>
-                )}
-              </tbody>
-            </Table>
-          )
-        }
-        <Button variant="dark" size="lg" onClick={() => setFormOpen(true)}>
-          <i className="fa-solid fa-person-circle-plus px-1" /> Add TPO Admin
-        </Button>
-        {
-          formOpen &&
+      {
+        loading ? (
+          <div className="flex justify-center h-72 items-center">
+            <i className="fa-solid fa-spinner fa-spin text-3xl" />
+          </div>
+        ) : (
           <>
-            <div className="bg-white flex justify-center w-full mt-4">
-              <div className='w-1/2 rounded-lg shadow px-10 py-3'>
-                <Form className='text-base' onSubmit={handleSubmit}>
-                  <h2>New TPO Admin</h2>
-                  <FloatingLabel className='my-3' label="Name">
-                    <Form.Control type="text" autoComplete="name" placeholder="Name" name='name' value={data.name || ''} onChange={handleDataChange} />
-                  </FloatingLabel>
-                  <FloatingLabel className='my-3' label="Email">
-                    <Form.Control type="email" autoComplete="email" placeholder="Email" name='email' value={data.email || ''} onChange={handleDataChange} />
-                  </FloatingLabel>
-                  <FloatingLabel className='my-3' label="Number">
-                    <Form.Control type="number" autoComplete="number" placeholder="Phone Number" name='number' value={data.number || ''} onChange={handleDataChange} />
-                  </FloatingLabel>
-                  <FloatingLabel className='my-3' label="Password">
-                    <Form.Control type="password" autoComplete="password" placeholder="Enter Initial Password" name='password' value={data.password || ''} onChange={handleDataChange} />
-                  </FloatingLabel>
-                  <button type="submit" className="flex items-center px-3 py-2 bg-blue-500 text-white rounded">
-                    <GrFormAdd className="mr-2 text-3xl" />
-                    Create New TPO User
-                  </button>
-                </Form>
+            {/*  any message here  */}
+            < Toast
+              show={showToast}
+              onClose={() => setShowToast(false)}
+              message={toastMessage}
+              delay={3000}
+              position="bottom-end"
+            />
+
+            <div className="ml-60 text-xl text-wrap">
+              <div className="container grid grid-cols-3 gap-3 mx-auto p-4">
+                {/* <div className="container flex justify-center items-start  gap-4 mx-auto p-4"> */}
+                {/* basic detail box  */}
+                <div className="bg-white p-6 rounded-lg shadow-md w-full">
+                  <div className="">
+                    <Form onSubmit={handleBasicDetailSubmit}>
+                      <Form.Group className="mb-3" controlId="formGroupName">
+                        <Form.Label>Full Name </Form.Label>
+                        <span className="text-red-500 px-1">*</span>
+                        <Form.Control type="text" placeholder="Enter Name" value={data.name || ''} name='name' onChange={handleBasicDetailChange} />
+                      </Form.Group>
+                      <Form.Group className="mb-3" controlId="formGroupEmail">
+                        <Form.Label>Email </Form.Label>
+                        <span className="text-red-500 px-1">*</span>
+                        <Form.Control disabled type="email" placeholder="Email Address" value={data.email || ''} name='email' />
+                      </Form.Group>
+                      <Form.Group className="mb-3" controlId="formGroupNumber">
+                        <Form.Label>Mobile Number </Form.Label>
+                        <span className="text-red-500 px-1">*</span>
+                        <Form.Control type="number" placeholder="Mobile Number" name='number' value={data.number || ''} onChange={handleBasicDetailChange} />
+                      </Form.Group>
+                      <button type="submit" className="flex items-center px-3 py-2 bg-blue-500 text-white rounded">
+                        <FaRegSave className="mr-2" />
+                        Submit
+                      </button>
+                    </Form>
+                  </div>
+                </div>
+
+                {/* photo box */}
+                <div className="bg-white p-6 rounded-lg shadow-md w-full">
+                  <div className="">
+                    <Form className='flex flex-col justify-center items-center gap-3 text-center'>
+                      <img src={BASE_URL + data.profile} alt="Profile Img" width="150" height="150" className='rounded-full' />
+                      <Form.Group controlId="formFile" className="mb-3" onChange={handlePhotoChange}>
+                        <Form.Label>{data.name}</Form.Label>
+                        <Form.Control type="file" accept=".jpg, .jpeg, .png" />
+                      </Form.Group>
+                    </Form>
+                  </div>
+                </div>
+
+                {/* password box */}
+                <div className="bg-white p-6 rounded-lg shadow-md w-full">
+                  <div className="">
+                    <Form onSubmit={handlePassUpdate} className='text-base'>
+                      <FloatingLabel className='my-3' label="Old Password">
+                        <Form.Control type="password" autoComplete="password" placeholder="Password" name='oldpass' value={passData.oldpass || ''} onChange={handlePassChange} />
+                      </FloatingLabel>
+                      <FloatingLabel className='my-3' label="New Password">
+                        <Form.Control type="password" autoComplete="password" placeholder="New Password" name='newpass' value={passData.newpass || ''} onChange={handlePassChange} />
+                      </FloatingLabel>
+                      <FloatingLabel className='my-3' label="Confirm New Password">
+                        <Form.Control type="password" autoComplete="password" placeholder="Confirm New Password" name='newcfmpass' value={passData.newcfmpass || ''} onChange={handlePassChange} />
+                      </FloatingLabel>
+                      <button type="submit" className="flex items-center px-3 py-2 bg-blue-500 text-white rounded">
+                        <RiKeyFill className="mr-2" />
+                        Change Password
+                      </button>
+                    </Form>
+                  </div>
+                </div>
+
+
               </div>
             </div>
           </>
-        }
-      </div>
+        )
+      }
+
     </>
-  );
+  )
 }
 
-export default AddTPO;
+export default Account
