3adbc638016d2e35d0b054fe95e77fcddb0a64a6
MoinMN <moinnaik45@eng.rizvi.edu.in>
2024-10-01 19:22:49 +0530

    added lazy adn suspense

--- frontend/src/components/Loading.jsx
+++ frontend/src/components/Loading.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
-import Img from '../assets/CPMS.png';
+import LogoVid from '../assets/CPMS.mp4';
 
 function LoadingComponent() {
   return (
     <div className="flex flex-col items-center justify-center h-screen w-full ">
-      <div className=" flex flex-col justify-between items-center">
-        <img src={Img} alt="Loading" className="w-36 h-36 mb-4 animate-pulse" />
+      <div className="flex flex-col justify-between items-center">
+        {/* <video src={LogoVid} alt="Loading" className="w-36 h-36 mb-4 animate-pulse" /> */}
+        <video width="350" height="350" autoPlay loop muted >
+          <source src={LogoVid} type="video/mp4" />
+        </video>
         <div className="flex items-center gap-2 mx-2">
-          <div className="w-8 h-8 border-4 border-blue-500 border-t-transparent border-solid rounded-full animate-spin"></div>
+          <div className="w-8 h-8 border-4 border-[#13325b] border-t-transparent border-solid rounded-full animate-spin"></div>
           <div className="">
-            <p className="text-xl font-medium pt-3 max-sm:text-base">Hold your sit tightly, we are coming...</p>
+            <p className="text-xl font-medium pt-3 max-sm:text-base text-[#13325b]">
+              Hold your sit tightly, we are coming...
+            </p>
           </div>
         </div>
       </div>
